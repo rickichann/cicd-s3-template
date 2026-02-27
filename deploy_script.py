913347e106e4d5c861100bb8d2e41a2c6c378e73
devrichan <rickichidayatullah@gmail.com>
2026-02-27 15:48:03 +0700

    Trigger workflow

--- deploy_script.py
+++ deploy_script.py
@@ -7,7 +7,13 @@ Replace this with your actual script logic
 def main():
     print("Hello from Python script!")
     print("This script will be deployed to S3")
+    print("gkgkgkkgk")
+    
     # Add your actual logic here
 
 if __name__ == "__main__":
     main()
+
+
+
+
