0e9f018269ef7e132655c16357204e182e261993
devrichan <rickichidayatullah@gmail.com>
2026-02-27 14:26:34 +0700

    first_commit

---
+++ deploy_script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Sample Python script for S3 deployment
+Replace this with your actual script logic
+"""
+
+def main():
+    print("Hello from Python script!")
+    print("This script will be deployed to S3")
+    # Add your actual logic here
+
+if __name__ == "__main__":
+    main()
